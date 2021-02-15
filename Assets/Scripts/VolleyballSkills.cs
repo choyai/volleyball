49b355339796dfad423174dc4ea9c1be3fc78caf
Chawakorn Chaichanawirote <choyaichaiyo@gmail.com>
2021-02-15 12:22:24 +0700

    bugfix

---
+++ Assets/Scripts/VolleyballSkills.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Volleyball
+{
+    public class VolleyballSkills : MonoBehaviour
+    {
+
+        // Power
+        public float power;
+        // Speed
+        public float speed;
+        // Accuracy
+        public float accuracy;
+
+    }
+
+}
